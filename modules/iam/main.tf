10e33ee5a685a74baa872a04f1330dc434f1538e
Harsha761-dev <sriharshakatiki761@gmail.com>
2024-11-14 19:25:02 +0530

    Create main.tf

---
+++ modules/iam/main.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "app_role" {
+  name = var.role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Effect = "Allow"
+      Sid    = ""
+    }]
+  })
+}
